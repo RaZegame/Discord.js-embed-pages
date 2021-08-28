9c0626e55a032eabaaea92d61d1ffc35d6c1c452
RaZegame <75721601+RaZegame@users.noreply.github.com>
2021-08-28 20:41:15 +0300

    Menu pagination

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,12 @@
 import { Client, Message, MessageEmbed } from "discord.js";
-
+interface pagesInt {
+    value: string;
+    embed: MessageEmbed;
+    label: string;
+    description?: string;
+    emoji?: string | Snowflake;
+}   
 declare module "discord.js-embed-pages" {
-    export function pages(message: Message, pages: MessageEmbed[], timeout: number, MovingButtonSytle: 'PRIMARY' | 'SECONDARY' | 'SUCCESS' | 'DANGER' )
+    export function pages(message: Message, pages: MessageEmbed[], timeout: number, MovingButtonSytle: 'PRIMARY' | 'SECONDARY' | 'SUCCESS' | 'DANGER'): void;
+    export function menuPages(message: Message, pages: pagesInt[], timeout: number)
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-exports.pages = async function (/** @type {Discord.Message} */ message, pages, timeout, MovingButtonSytle) {
+exports.pages = async (/** @type {Discord.Message} */ message, pages, timeout, MovingButtonSytle) => {
 
 
     const pageMovingButtons1 = new Discord.MessageButton()
@@ -25,9 +25,9 @@ exports.pages = async function (/** @type {Discord.Message} */ message, pages, t
     const col = await m.channel.createMessageComponentCollector({ componentType: "BUTTON", dispose: true, filter: (button) => button.user.id === message.author.id, time: timeout, idle: timeout / 2 })
     col.on("collect", (int) => {
         if (int.isButton()) {
-            if(int.customId === "del_button") {
+            if (int.customId === "del_button") {
                 col.stop()
-                m.delete().catch(() => {});
+                m.delete().catch(() => { });
             }
             if (int.customId === "back_button_embed") {
                 if (currentPage - 1 < 0) {
@@ -50,11 +50,40 @@ exports.pages = async function (/** @type {Discord.Message} */ message, pages, t
     });
     col.on("end", () => {
         const disrow = new Discord.MessageActionRow().addComponents(pageMovingButtons.components[0].setDisabled(true), pageMovingButtons.components[1].setDisabled(true), pageMovingButtons.components[2].setDisabled(true));
-        if(m.editable) {
-            m.edit({ embeds: [pages[0]], components: [disrow] }).catch(() => {})
+        if (m.editable) {
+            m.edit({ embeds: [pages[0]], components: [disrow] }).catch(() => { })
         } else {
             return;
         }
     });
 
-}
+};
+
+exports.menuPages = async (/** @type {Discord.Message} */ message, pages, timeout) => {
+
+    const Menu = new Discord.MessageSelectMenu()
+        .setCustomId("sel_menu_pages")
+        .setPlaceholder("Select something!")
+        .setMaxValues(pages.length)
+        .addOptions(pages);
+
+    const msg = await message.channel.send({ embeds: [pages[0].embed], components: [new Discord.MessageActionRow().addComponents(Menu)] })
+    const col = await message.channel.createMessageComponentCollector({ componentType: "SELECT_MENU", filter: (int) => int.user.id === message.author.id, dispose: true, time: timeout, idle: timeout / 2 });
+    col.on("collect", async (int) => {
+        if (int.isSelectMenu()) {
+            for await (const value of int.values) {
+                for (const page of pages) {
+                    if (page.value === value) {
+                        msg.edit({ embeds: [page.embed] })
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+    });
+    col.once("end", async () => {
+        const disabledActionrow = new Discord.MessageActionRow().addComponents(Menu.setDisabled(true));
+        await msg.edit({ embeds: [pages[0].embed], components: [disabledActionrow] })
+    });
+};
