a3ff71ea14877f1ee5d38442f0a82a9e6ac8711b
RaZe <75721601+RaZegame@users.noreply.github.com>
2021-08-28 20:54:57 +0300

    Example bot

--- exampleBot/index.js
+++ exampleBot/index.js
@@ -3,8 +3,33 @@ const depb = require("../index.js");
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] })
 
 client.on("messageCreate", (message) => {
-    if (message.content.startsWith("!help")) {
-        depb.pages(message, [new Discord.MessageEmbed().setTitle("1"), new Discord.MessageEmbed().setTitle("2"), new Discord.MessageEmbed().setTitle("3")], 100 * 60 * 1000, 'PRIMARY');
+    if (message.content === "!help") {
+        depb.pages(message, [new Discord.MessageEmbed().setTitle("1"), new Discord.MessageEmbed().setTitle("2"), new Discord.MessageEmbed().setTitle("3")], 3 * 60 * 1000, 'PRIMARY');
+    }
+    if (message.content.startsWith("!help 2")) {
+        depb.menuPages(message, [
+            {
+                value: "1",
+                embed: new Discord.MessageEmbed().setTitle("1"),
+                label: "number 1",
+                description: "It is a number 1",
+                emoji: "1️⃣",
+            },
+            {
+                value: "2",
+                embed: new Discord.MessageEmbed().setTitle("2"),
+                label: "number 2",
+                description: "It is a number 2",
+                emoji: "2️⃣",
+            },
+            {
+                value: "3",
+                embed: new Discord.MessageEmbed().setTitle("3"),
+                label: "number 3",
+                description: "It is a number 3",
+                emoji: "3️⃣",
+            }
+        ], 1 * 60 * 1000)
     }
 });
 
